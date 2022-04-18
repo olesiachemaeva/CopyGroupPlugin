e33486198a3d68749db1086e4c632aa3ec7d87a1
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-04-19 08:06:58 +1000

    Ветвь урока 3 Плагин часть 2

--- CopyGroupPlugin/Class1.cs
+++ CopyGroupPlugin/Class1.cs
@@ -15,22 +15,35 @@ namespace CopyGroupPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDoc =  commandData.Application.ActiveUIDocument;
-            Document doc = uiDoc.Document;
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
 
-           
- //попросить пользователя выбрать группу для копирования
-            Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов"); // получили ссылку на выбранную пользователем группу объектов
-            Element element = doc.GetElement(reference);
-            Group group = element as Group; // тот объект по кт пользователь щелкнул получили и преобразовали к типу Group
 
-            //попросим пользователя выбрать какую то точку 
-            XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+                //попросить пользователя выбрать группу для копирования
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов"); // получили ссылку на выбранную пользователем группу объектов
+                Element element = doc.GetElement(reference);
+                Group group = element as Group; // тот объект по кт пользователь щелкнул получили и преобразовали к типу Group
 
-            Transaction transaction = new Transaction(doc);
-            transaction.Start("Копирование группы объектов");
-            doc.Create.PlaceGroup(point, group.GroupType);
-            transaction.Commit();
+                //попросим пользователя выбрать какую то точку 
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("Копирование группы объектов");
+                doc.Create.PlaceGroup(point, group.GroupType);
+                transaction.Commit();
+            }
+            catch(Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch(Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
 
             return Result.Succeeded;
 
