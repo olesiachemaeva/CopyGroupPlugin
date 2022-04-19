d4de4632fd778e2759746a77221fa5faa5680f49
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-04-19 22:46:19 +1000

    Задание 3 по уроку

--- CopyGroupPlugin/Class1.cs
+++ CopyGroupPlugin/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -20,11 +21,16 @@ namespace CopyGroupPlugin
                 UIDocument uiDoc = commandData.Application.ActiveUIDocument;
                 Document doc = uiDoc.Document;
 
-
+                GroupPickFillter groupPickFillter = new GroupPickFillter();
                 //попросить пользователя выбрать группу для копирования
-                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов"); // получили ссылку на выбранную пользователем группу объектов
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFillter, "Выберите группу объектов"); // получили ссылку на выбранную пользователем группу объектов
                 Element element = doc.GetElement(reference);
                 Group group = element as Group; // тот объект по кт пользователь щелкнул получили и преобразовали к типу Group
+                XYZ goupCenter = GetElementCenter(group);
+                Room room = GetRoomByOption(doc, goupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = goupCenter - roomCenter;
+
 
                 //попросим пользователя выбрать какую то точку 
                 XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
@@ -40,7 +46,7 @@ namespace CopyGroupPlugin
             }
             catch(Exception ex)
             {
-                message = ex.Message;
+                message = ex.Message;  //из-за чего произошла ошибка - текст исключения
                 return Result.Failed;
             }
 
@@ -48,5 +54,43 @@ namespace CopyGroupPlugin
             return Result.Succeeded;
 
         }
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null); // рамка в 3х измерениях
+            return (bounding.Max + bounding.Min) / 2;
+        }
+        public Room GetRoomByOption(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach(Element e in collector)
+            {
+                Room room = e as Room;
+                if (room! = null)
+                {
+                    if(room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
     }
+    public class GroupPickFillter : ISelectionFilter //фильтр
+    {
+        public bool AllowElement(Element elem) 
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+                return true;
+            else
+                return false;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+    
 }
